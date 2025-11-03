2c3a31645e011e6bebd249678c19aebffb6b5987
vipin yadav <cy7795151@gmail.com>
2025-11-03 19:10:39 +0530

    feat: design and style LandingPage component
    - Implemented responsive LandingPage layout with navigation and hero section
    - Applied custom CSS for typography and background
    - Added navigation links and styled call-to-action button

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
+import '../App.css';
 function LandingPage() {
   return (
-    <div><p>This is Landing Page</p></div>
+    <>
+    <div className="landingPageContainer">
+        <nav>
+            <div className="navHeader">
+               <h2>Apna Video Call</h2>
+            </div>
+            <div className="navlist">
+               <p>Join as Guest</p>
+               <p>Register</p>
+               <div role="button">
+                <p>Login</p>
+               </div>
+            </div>
+        </nav>
+
+          <div className="landingMainContainer">
+          
+          <div>
+            <h1><span style={{ color: "orange" }}>Connect</span> with you loved once</h1>
+            <p>Cover a distance by Vipin's Video conferencing app</p>
+            <div role="button">
+                <Link to={"/home"}>Get Started</Link>
+            </div>
+          </div>
+          
+          <div>
+            <img src="../../public/mobile.png" alt="hero image"></img>
+          </div>
+   </div>
+    </div>
+
+ 
+</>
   )
 }
 
